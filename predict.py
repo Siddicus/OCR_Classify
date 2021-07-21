bdd8e2f8ba0accc8c9d28afd09d1bbda80f1fe3d
Siddicus <70978638+Siddicus@users.noreply.github.com>
2021-07-21 03:48:21 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,1 +1,26 @@
+# from model import my_model
 
+
+def main(args):
+    # Define your training procedure here
+
+    # script arguments are accessible as follows:
+    # img_path = args.img_path
+    # ckpt_path = args.checkpoint_path
+    pass
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(description='Inference script',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('img_path', type=str, help='path to the image')
+    parser.add_argument('checkpoint_path', type=str, help='path to your model checkpoint')
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
